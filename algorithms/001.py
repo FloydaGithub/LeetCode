aad8da5e5f2f5fc8b8c2c68480f073ceb20ad516
Floyda <floyda@163.com>
2019-03-08 14:40:40 +0800

    update

---
+++ algorithms/001.py
@@ -0,0 +1,30 @@
+# https://leetcode-cn.com/problems/two-sum/
+'''
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+'''
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        _hash = {}
+        b = -1
+        for x in nums:
+            b += 1
+            _target = target - x
+            a = _hash.get(_target, None)
+            if not a is None:
+                return [a, b]
+            _hash[x] = b
+        return None
